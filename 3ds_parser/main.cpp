e9c0d5a08f70f1dae53809ed581b34a37eccb469
Beau Carlborg <bcarlborg@gmail.com>
2019-01-28 12:01:29 -0700

    initial cmake added and function cmake for the system thus far

---
+++ 3ds_parser/main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Beau Carlborg on 2019-01-28.
+//
+#include <stdio.h>
+#include <lib3ds.h>
+
+int main(int argc, char **argv) {
+    printf("this is a test string\n");
+    return 0;
+}
+
+
+
