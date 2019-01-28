d1ada419fe8d2b5b3a09a9cf5ee692e3fcbbcd40
Beau Carlborg <bcarlborg@gmail.com>
2019-01-28 14:33:59 -0700

    retrieving total number of meshes

--- 3ds_parser/main.cpp
+++ 3ds_parser/main.cpp
@@ -1,13 +1,33 @@
 //
 // Created by Beau Carlborg on 2019-01-28.
 //
+
+#include "main.h"
 #include <stdio.h>
-#include <lib3ds.h>
+
+
 
 int main(int argc, char **argv) {
-    printf("this is a test string\n");
+    Lib3dsFile *new_file = read_3ds_file(argv[1]);
+    printf("\nThe file was parsed in a half sensible way!\nHere are the total number of meshes: %d\n", new_file->nmeshes);
+
     return 0;
 }
 
 
+Lib3dsFile* read_3ds_file(const char* file_path) {
 
+    printf("***********FILE PATH FROM MAIN.CPP: %s", file_path);
+    Lib3dsFile *parsed_file = lib3ds_file_open(file_path);
+
+    if (!parsed_file) {
+        printf("#####ERROR#####\n##Failed to open file %s##", file_path);
+    }
+    else {
+        printf("successfully read file, there are %d meshes in the file\n", parsed_file->nmeshes);
+    }
+
+    return parsed_file;
+
+
+}
---
+++ 3ds_parser/main.h
@@ -0,0 +1,14 @@
+//
+// Created by Beau Carlborg on 2019-01-28.
+//
+
+
+#include <lib3ds.h>
+
+#ifndef _parse3ds_main_h
+#define _parse3ds_main_h
+
+// Takes a path to a
+Lib3dsFile* read_3ds_file(const char* file_path);
+
+#endif
