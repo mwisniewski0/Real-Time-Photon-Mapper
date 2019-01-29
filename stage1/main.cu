766db744fdaafedcab8b85cfe34224b7546f79ba
Sam Kottler <kottler.sam@gmail.com>
2019-01-29 10:52:13 -0700

    Added very simple photon tracing with cornel box for visualization.
    Resolves #18

--- stage1/main.cu
+++ stage1/main.cu
@@ -4,25 +4,98 @@
 #include <vector>
 
 #include <cuda_runtime.h>
+#include <curand.h>
+#include <curand_kernel.h>
 
 #include "cutil_math.h"
 
 #define WIDTH 500
 #define HEIGHT 500
-#define NUM_PHOTONS WIDTH*HEIGHT
+#define NUM_PHOTONS (1<<20)
 
 struct Photon{
     float3 pos;
     float3 power;
 };
 
-__global__ void getPhotonsKernel(Photon* photonList){
-    uint x = blockIdx.x*blockDim.x + threadIdx.x;
-    uint y = blockIdx.y*blockDim.y + threadIdx.y;
+struct PointLight{
+    float3 pos;
+    float3 power;
+};
 
-    uint idx = y*WIDTH + x;
-    photonList[idx].pos = make_float3(2*(float)x/WIDTH - 1,1, (float)2*y/HEIGHT-1);
-    photonList[idx].power = make_float3((float)x/WIDTH, (float)y/HEIGHT,0);
+struct Triangle{
+    float3 v0,v1,v2;
+    float3 color;
+    __device__ float intersect(float3& point, float3& direction) const{
+	float3 tvec = point - v0;
+	float3 pvec = cross(direction, v2-v0);
+	float det = dot(v1-v0, pvec);
+        
+	det = __fdividef(1.0, det);
+
+	float u = dot(tvec, pvec)*det;
+	if (u < 0 || u > 1)
+	    return -1e20;
+
+	float3 qvec = cross(tvec, v1-v0);
+
+	float v = dot(direction, qvec) * det;
+
+	if (v < 0 || (u+v) > 1)
+	    return -1e20;
+
+	return dot(v2-v0, qvec) * det;
+    }
+};
+
+
+__constant__ PointLight pointLights[] = {
+    {{0,0.95,0}, {1000000,1000000,1000000}}
+};
+
+__constant__ Triangle triangles[] = {
+    {{-1,-1,-1},{1,-1,-1},{-1,-1,1},{0.9,0.9,0.9}},
+    {{1,-1,-1},{1,-1,1},{-1,-1,1},{0.9,0.9,0.9}},
+    {{-1,-1,-1},{-1,1,-1},{1,1,-1},{0.9,0.9,0.9}},
+    {{-1,-1,-1},{1,1,-1},{1,-1,-1},{0.9,0.9,0.9}},
+    {{-1,1,1},{1,1,1},{1,1,-1},{0.9,0.9,0.9}},
+    {{-1,1,1},{1,1,-1},{-1,1,-1},{0.9,0.9,0.9}},
+    {{-1,-1,1},{-1,1,1},{-1,1,-1},{0.9,0.2,0.2}},
+    {{-1,-1,1},{-1,1,-1},{-1,-1,-1},{0.9,0.2,0.2}},
+    {{1,-1,1},{1,1,1},{1,1,-1},{0.2,0.9,0.2}},
+    {{1,-1,1},{1,1,-1},{1,-1,-1},{0.2,0.9,0.2}}
+};
+
+__global__ void getPhotonsKernel(Photon* photonList){
+    uint idx = blockIdx.x*blockDim.x + threadIdx.x;
+
+    curandState randState;
+    curand_init(idx,0,0,&randState);
+
+    float cosPhi = curand_uniform(&randState)*2 - 1;
+    float sinPhi = sqrtf(1-cosPhi*cosPhi);
+    float theta = curand_uniform(&randState)*2*M_PI;
+
+    float3 origin = pointLights[0].pos;
+    float3 direction = make_float3(sinPhi*cosf(theta), sinPhi*sinf(theta), cosPhi);
+
+    float t = 1e20;
+    uint triIdx = 0;
+    uint numTriangles = sizeof(triangles) / sizeof(Triangle);
+    for (uint i = numTriangles; i--; ){
+	float d = triangles[i].intersect(origin, direction);
+	if (d>0 && d<t){
+	    t = d;
+	    triIdx = i;
+	}
+    }
+    if (t < 1e19){
+	photonList[idx].pos = origin + direction*t;
+	photonList[idx].power = triangles[triIdx].color*pointLights[0].power/NUM_PHOTONS;
+    }
+    else{
+	photonList[idx].pos = make_float3(1e20,1e20,1e20);
+    }
 }
 
 void writeTestToFile(std::vector<Photon> photons, std::string filename){
@@ -34,9 +107,9 @@ void writeTestToFile(std::vector<Photon> photons, std::string filename){
 	    int x = photon.pos.x/2/(-photon.pos.z + 2)*WIDTH + WIDTH/2;
 	    int y = -photon.pos.y/2/(-photon.pos.z + 2)*HEIGHT + HEIGHT/2;
 	    uint idx = y*WIDTH + x;
-	    sums[3*idx+0] += (int)256*photon.power.x;
-	    sums[3*idx+1] += (int)256*photon.power.y;
-	    sums[3*idx+2] += (int)256*photon.power.z;
+	    sums[3*idx+0] += (int)255*photon.power.x;
+	    sums[3*idx+1] += (int)255*photon.power.y;
+	    sums[3*idx+2] += (int)255*photon.power.z;
 	    counts[idx] += 1;
 	}
     }
@@ -68,10 +141,8 @@ int main(){
     Photon* photonList_d;
  
     cudaMalloc(&photonList_d, photonList_h.size()*sizeof(Photon));
-    dim3 block(8,8,1);
-    dim3 grid(WIDTH/block.x, HEIGHT/block.y, 1);
     
-    getPhotonsKernel<<<grid, block>>>(photonList_d);
+    getPhotonsKernel<<<NUM_PHOTONS/64, 64>>>(photonList_d);
     cudaDeviceSynchronize();
     cudaMemcpy(photonList_h.data(), photonList_d,
 	       photonList_h.size()*sizeof(Photon), cudaMemcpyDeviceToHost);
