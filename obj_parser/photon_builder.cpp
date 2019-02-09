b5a8a93df64c2ad7fce294f97fa5dc1f03ca2e78
Beau Carlborg <bcarlborg@gmail.com>
2019-02-09 14:25:07 -0700

    allowing writing of photon file using floats not float3s

--- obj_parser/photon_builder.cpp
+++ obj_parser/photon_builder.cpp
@@ -2,16 +2,149 @@
 // Created by Beau Carlborg on 2019-02-09.
 //
 
-// C program for writing
-// struct to file
-
-
 #include "./photon_builder.h"
 
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+
+Binary_Material construct_binary_material(Material material_in) {
+    Binary_Material bin_material;
+
+    bin_material.diffuse[0] = material_in.diffuse.x;
+    bin_material.diffuse[1] = material_in.diffuse.y;
+    bin_material.diffuse[1] = material_in.diffuse.z;
+    
+    bin_material.specular[0] = material_in.specular.x;
+    bin_material.specular[1] = material_in.specular.y;
+    bin_material.specular[1] = material_in.specular.z;
+    
+    bin_material.transmittance[0] = material_in.transmittance.x;
+    bin_material.transmittance[1] = material_in.transmittance.y;
+    bin_material.transmittance[1] = material_in.transmittance.z;
+
+    bin_material.shininess = material_in.shininess;
+
+    bin_material.type = material_in.type;
+
+    return bin_material;
+}
+
+Material deconstruct_binary_material(Binary_Material material_in) {
+    Material material;
+
+    material.diffuse = make_float3(material_in.diffuse[0],
+                                   material_in.diffuse[1],
+                                   material_in.diffuse[2]
+    );
+
+    material.specular = make_float3(material_in.specular[0],
+                                   material_in.specular[1],
+                                   material_in.specular[2]
+    );
+
+    material.transmittance = make_float3(material_in.transmittance[0],
+                                   material_in.transmittance[1],
+                                   material_in.transmittance[2]
+    );
+
+    material.shininess = material_in.shininess;
+
+    material.type = material.type;
+
+    return material;
+
+}
+
+
+Binary_Triangle construct_binary_triangle(Triangle triangle_in) {
+    Binary_Triangle bin_triangle;
+
+    bin_triangle.p[0] = triangle_in.p.x;
+    bin_triangle.p[1] = triangle_in.p.y;
+    bin_triangle.p[2] = triangle_in.p.z;
+
+    bin_triangle.v0[0] = triangle_in.v0.x;
+    bin_triangle.v0[1] = triangle_in.v0.y;
+    bin_triangle.v0[2] = triangle_in.v0.z;
+
+    bin_triangle.v1[0] = triangle_in.v1.x;
+    bin_triangle.v1[1] = triangle_in.v1.y;
+    bin_triangle.v1[2] = triangle_in.v1.z;
+
+    bin_triangle.normal[0] = triangle_in.normal.x;
+    bin_triangle.normal[1] = triangle_in.normal.y;
+    bin_triangle.normal[2] = triangle_in.normal.z;
+
+    bin_triangle.v0vt[0] = triangle_in.v0vt.x;
+    bin_triangle.v0vt[1] = triangle_in.v0vt.y;
+    bin_triangle.v0vt[2] = triangle_in.v0vt.z;
+
+    bin_triangle.v1vt[0] = triangle_in.v1vt.x;
+    bin_triangle.v1vt[1] = triangle_in.v1vt.y;
+    bin_triangle.v1vt[2] = triangle_in.v1vt.z;
+
+    bin_triangle.v2vt[0] = triangle_in.v2vt.x;
+    bin_triangle.v2vt[1] = triangle_in.v2vt.y;
+    bin_triangle.v2vt[2] = triangle_in.v2vt.z;
+
+    bin_triangle.bin_material = construct_binary_material(triangle_in.material);
+
+    return bin_triangle;
+}
+
+
+Triangle deconstruct_binary_triangle(Binary_Triangle triangle_in) {
+    Triangle triangle;
+
+    triangle.p = make_float3(triangle_in.p[0],
+                             triangle_in.p[1],
+                             triangle_in.p[2]
+    );
+
+
+    triangle.v0 = make_float3(triangle_in.v0[0],
+                             triangle_in.v0[1],
+                             triangle_in.v0[2]
+    );
+
+    triangle.v1 = make_float3(triangle_in.v1[0],
+                             triangle_in.v1[1],
+                             triangle_in.v1[2]
+    );
+
+
+    triangle.normal = make_float3(triangle_in.normal[0],
+                             triangle_in.normal[1],
+                             triangle_in.normal[2]
+    );
+
+
+    triangle.v0vt = make_float3(triangle_in.v0vt[0],
+                             triangle_in.v0vt[1],
+                             triangle_in.v0vt[2]
+    );
+
+
+    triangle.v1vt = make_float3(triangle_in.v1vt[0],
+                             triangle_in.v1vt[1],
+                             triangle_in.v1vt[2]
+    );
+
+    triangle.v2vt = make_float3(triangle_in.v2vt[0],
+                             triangle_in.v2vt[1],
+                             triangle_in.v2vt[2]
+    );
+
+
+    triangle.material = deconstruct_binary_material(triangle_in.bin_material);
+
+    return triangle;
+}
+
+
 
 
 void triangle_write(std::vector<Triangle> *triangles) {
@@ -26,7 +159,8 @@ void triangle_write(std::vector<Triangle> *triangles) {
     }
 
     for (int i = 0; i < triangles->size(); ++i) {
-        fwrite(&(triangles->at(i)), sizeof(Triangle), 1, outfile);
+        Binary_Triangle binary_triangle = construct_binary_triangle(triangles->at(i));
+        fwrite(&(binary_triangle), sizeof(Binary_Triangle), 1, outfile);
     }
 
     if(fwrite != 0)
@@ -41,7 +175,7 @@ void triangle_write(std::vector<Triangle> *triangles) {
 void triangle_read(std::vector<Triangle> *triangles) {
     FILE *infile;
 
-    Triangle input;
+    Binary_Triangle input;
 
     // Open person.dat for reading
     infile = fopen ("/Users/beaucarlborg/CLionProjects/Real-Time-Photon-Mapper/obj_parser/triangle.dat", "r");
@@ -52,62 +186,11 @@ void triangle_read(std::vector<Triangle> *triangles) {
     }
 
     // read file contents till end of file
-    while(fread(&input, sizeof(struct Triangle), 1, infile)) {
-        triangles->push_back(input);
+    while(fread(&input, sizeof(Binary_Triangle), 1, infile)) {
+        Triangle triangle = deconstruct_binary_triangle(input);
+
+        triangles->push_back(triangle);
     }
-
-    // close file
-    fclose (infile);
-}
-
-
-
-
-
-
-void person_write() {
-    FILE *outfile;
-
-    // open file for writing
-    outfile = fopen ("/Users/beaucarlborg/CLionProjects/Real-Time-Photon-Mapper/obj_parser/person.dat", "w");
-    if (outfile == NULL)
-    {
-        fprintf(stderr, "\nError opend file\n");
-        exit (1);
-    }
-
-    struct person input1 = {1, "rohan", "sharma"};
-    struct person input2 = {2, "mahendra", "dhoni"};
-
-    // write struct to file
-    fwrite (&input1, sizeof(struct person), 1, outfile);
-    fwrite (&input2, sizeof(struct person), 1, outfile);
-
-    if(fwrite != 0)
-        printf("contents to file written successfully !\n");
-    else
-        printf("error writing file !\n");
-
-    // close file
-    fclose (outfile);
-}
-
-void person_read() {
-    FILE *infile;
-    struct person input;
-
-    // Open person.dat for reading
-    infile = fopen ("/Users/beaucarlborg/CLionProjects/Real-Time-Photon-Mapper/obj_parser/person.dat", "r");
-    if (infile == NULL)
-    {
-        fprintf(stderr, "\nError opening file\n");
-        exit (1);
-    }
-
-    // read file contents till end of file
-    while(fread(&input, sizeof(struct person), 1, infile))
-        printf ("id = %d name = %s %s\n", input.id,
-                input.fname, input.lname);
 
     // close file
     fclose (infile);
--- obj_parser/photon_builder.h
+++ obj_parser/photon_builder.h
@@ -14,24 +14,60 @@
 // struct to file
 
 #include "../common/geometry.h"
+#include "../common/cutil_math.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-// a struct to read and write
-struct person
+typedef struct Binary_Material
 {
-    int id;
-    char fname[20];
-    char lname[20];
-};
+    float diffuse[3];
+    float specular[3];
+    float transmittance[3];
+    float shininess;
+    int type;
+} Binary_Material;
+
+typedef struct Binary_Triangle
+{
+    float p[3];
+    float v0[3];
+    float v1[3];
+    float normal[3];
+
+    float v0vt[3];
+    float v1vt[3];
+    float v2vt[3];
+
+    Binary_Material bin_material;
+} Binary_Triangle;
+
+
+Binary_Material construct_binary_material(Material material_in);
+
+Material deconstruct_binary_material(Binary_Material material_in);
+
+Binary_Triangle construct_binary_triangle(Triangle triangle_in);
+
+Triangle deconstruct_binary_triangle(Binary_Triangle triangle_in);
 
 
 void triangle_write(std::vector<Triangle> *triangles);
 void triangle_read(std::vector<Triangle> *triangles);
 void person_write();
 void person_read();
+/*
+ *
+struct Triangle : public Shape {
+	float3 p;  // v0
+	float3 v0;   // v0->v1
+	float3 v1;   // v0->v2
+	float3 normal; //precompute and store. may not be faster needs testing
+	Material material;
+
+	float3 v0vt;
+    float3 v1vt;
+    float3 v2vt;
+ *
+ */
+
 
 
 #endif //PHOTON_PHOTON_BUILDER_H
